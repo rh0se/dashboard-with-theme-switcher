53c2cabcbd8fa27f9b3a70c5118c3298d0259a0b
Rhoda Ojetola <momirhoda@gmail.com>
2024-12-11 10:50:45 +0100

    adding light theme styles to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,13 @@ const { isDarkTheme, toggleTheme } = useTheme();
   return (
     <>
     <div>
-    <div className='flex justify-between items-center'>
+    <div className='flex md:flex-row gap-4 flex-col md:justify-between md:items-center'>
         <div className='flex flex-col'>
-        <h1>Social Media Dashboard</h1>
-        <p>Total Followers: 23,004</p>
+        <h1 className={`${isDarkTheme? "text-white": "text-DarkGrayishBlue"} text-2xl font-extrabold` } >Social Media Dashboard</h1>
+        <p className={`${isDarkTheme ? "text-DesaturatedBlue" : "text-DarKGrayishBlue"} text-xs font-bold`}>Total Followers: 23,004</p>
         </div>
-        <div className='flex items-center' >
-            <p className='mr-7'>{`${isDarkTheme? "Dark Mode":"Light Mode"}`}</p>
+        <div className='flex md:p-0 pt-2 items-center justify-between md:border-0 border-t-2 border-DesaturatedBlue' >
+            <p className={`${isDarkTheme ? "text-DesaturatedBlue" : "text-DarKGrayishBlue"} text-xs mr-3 font-bold`}>{`${isDarkTheme? "Dark Mode":"Light Mode"}`}</p>
             <button className='text-4xl' onClick={toggleTheme}><IoToggleSharp className={`${isDarkTheme? 'rotate-180 text-darkToggle1': 'rotate-0 text-lightToggle1'}`}></IoToggleSharp></button>
         </div>    
     </div>
